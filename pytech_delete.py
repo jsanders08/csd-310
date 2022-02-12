7f79afc56a936d3e950fda96082e00da8eceed1d
jsanders08 <82988477+jsanders08@users.noreply.github.com>
2022-02-12 13:57:37 -0800

    Update pytech_delete.py

--- pytech_delete.py
+++ pytech_delete.py
@@ -1,1 +1,34 @@
+from pymongo import MongoClient
+url = "mongodb://admin:admin@cluster0-shard-00-00.b75yo.mongodb.net:27017,cluster0-shard-00-01.b75yo.mongodb.net:27017,cluster0-shard-00-02.b75yo.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-11zqk8-shard-0&authSource=admin&retryWrites=true&w=majority"
+client = MongoClient(url)
 
+db = client.pytech
+students = db.students
+student_list = students.find({})
+
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+for doc in student_list:
+    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+test_doc = {
+    "student_id": "1010",
+    "first_name": "Corky",
+    "last_name": "Romano"
+}
+
+test_doc_id = students.insert_one(test_doc).inserted_id
+print("\n  -- INSERT STATEMENTS --")
+print("  Inserted student record into the students collection with document_id " + str(test_doc_id))
+
+student_test_doc = students.find_one({"student_id": "1010"})
+print("\n  -- DISPLAYING STUDENT TEST DOC -- ")
+print("  Student ID: " + student_test_doc["student_id"] + "\n  First Name: " + student_test_doc["first_name"] + "\n  Last Name: " + student_test_doc["last_name"] + "\n")
+
+deleted_student_test_doc = students.delete_one({"student_id": "1010"})
+new_student_list = students.find({})
+print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+
+for doc in new_student_list:
+    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+
+input("\n\n  End of program, press any key to continue...")
